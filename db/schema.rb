a9d9612b087f679b96616abd5c3f6be9e7125315
Shreya Bastikar <sbastik@example.com>
2014-09-24 13:21:43 -0400

    Admins complete, Jobseekers half done

--- db/schema.rb
+++ db/schema.rb
@@ -32,5 +32,14 @@ ActiveRecord::Schema.define(version: 20140922224546) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-
+  create_table "jobs", force: true do |t|
+    t.string   "title"
+    t.string   "description"
+    t.datetime "application_deadline"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string "category_id"
+    t.string "tags_id"
+    t.string "employer_id"
+  end
 end
